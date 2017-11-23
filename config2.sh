b30c968d74bed3eb47533fb563199a73a0eee983
Michael Nielsen <mn@michaelnielsen.org>
2017-11-22 21:17:44 -0800

    ...

--- config2.sh
+++ config2.sh
@@ -1,6 +1,5 @@
 # Configuration information for running on Google Compute Cloud. Part
 # II.
-git clone http://github.com/mnielsen/rmnist.git
 conda install pytorch torchvision cuda80 -c soumith
 conda install ipython
 conda install matplotlib
