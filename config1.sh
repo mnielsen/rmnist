5ff2088aa5cb2e7067aed59aa3f53627709727eb
Michael Nielsen <mn@michaelnielsen.org>
2017-11-22 20:52:47 -0800

    Configuration for Google Compute cloud

---
+++ config1.sh
@@ -0,0 +1,5 @@
+# Configuration information for running on Google Compute Cloud. Part
+# I. SSH session then needs to be restarted, and config2.sh run.
+wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
---
+++ config2.sh
@@ -0,0 +1,7 @@
+# Configuration information for running on Google Compute Cloud. Part
+# II.
+git clone http://github.com/mnielsen/rmnist.git
+conda install pytorch torchvision cuda80 -c soumith
+conda install ipython
+conda install matplotlib
+
